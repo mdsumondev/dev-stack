7c481939d18f26149f4265056d6d86b75b741e8d
Md.Sumon <sumonahamed5566@gmail.com>
2026-09-11 01:25:08 +0600

    large device header

---
+++ src/Component/Header.tsx
@@ -0,0 +1,74 @@
+import logo from "../assets/logo-text.png";
+const Header = () => {
+  return (
+    <div>
+      <div className="container flex justify-between items-center mx-auto">
+        <div className="logo">
+          <a href="/">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+        <div className="menu-bar">
+          <ul className="flex items-center gap-[29px] py-6">
+            <li>
+              <a
+                className="text-base font-medium hover:text-[#D91B7E] duration-150 ease-in-out"
+                href="/"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-base font-medium hover:text-[#D91B7E] duration-150 ease-in-out"
+                href="#"
+              >
+                Technologies
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-base font-medium hover:text-[#D91B7E] duration-150 ease-in-out"
+                href="#"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-base font-medium hover:text-[#D91B7E] duration-150 ease-in-out"
+                href="#"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-base font-medium hover:text-[#D91B7E] duration-150 ease-in-out"
+                href="#"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-5 items-center">
+          <a
+            className="text-base font-medium hover:text-[#D91B7E] duration-150 ease-in-out"
+            href="#"
+          >
+            Sign In
+          </a>
+          <a
+            className="text-base font-medium text-white bg-[#D91B7E] rounded-3xl py-[10px] px-5"
+            href="#"
+          >
+            Sign Up
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
